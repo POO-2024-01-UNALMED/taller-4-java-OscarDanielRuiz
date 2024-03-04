cc678940f24f7438f84cc217826ca7da1cb846de
OscarDanielRuiz <159485737+OscarDanielRuiz@users.noreply.github.com>
2024-03-04 12:43:17 -0500

    prueba1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,15 +2,20 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
-    String nombre;
+    long cedula = 1;
+    private String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        //cedula = 3;
     }
 
+    public Persona() {
+        this.nombre = "";
+        this.cedula = 0;
+    }
+    
     public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -30,7 +35,7 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
         totalPersonas++;
     }
     
